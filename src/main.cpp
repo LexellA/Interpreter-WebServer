ece280c61885dc873dd7cc5c7c8027aa9589d4ad
LexellA <2773564406@qq.com>
2024-07-31 21:30:06 +0800

    chore: Increase server thread count to 16 for improved performance

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ using namespace server;
 
 int main(int argc, char** argv)
 {
-    Server server(3411, 3, 60000, true, 8, 
+    Server server(3411, 3, 60000, true, 16, 
                 "root", "localhost", 3306, "root", "123456", "chatbot",
                 15000, 1000, Log::Level::DEBUG, "./scripts/script_store.txt");
 
